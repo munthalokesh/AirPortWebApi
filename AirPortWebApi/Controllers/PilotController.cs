7677ee9ffd75eaa10f31f2ea12104035afa6e5d2
Lokesh Muntha <lokesh.muntha@techwave.net>
2023-08-01 17:05:46 +0530

    Add project files.

---
+++ AirPortWebApi/Controllers/PilotController.cs
@@ -0,0 +1,45 @@
+﻿using AirPortWebApi.Models.DataLayer;
+using AirPortWebApi.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AirPortWebApi.Controllers
+{
+    public class PilotController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IHttpActionResult AddPilot([FromBody] PilotCls p)
+        {
+            PilotDl pd= new PilotDl();
+            string s=pd.AddingPilot(p);
+            return Ok(s);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
