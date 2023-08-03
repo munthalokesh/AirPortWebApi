397a6993a81e63f1cc4641023920be75ed5716c4
Lokesh Muntha <lokesh.muntha@techwave.net>
2023-08-03 12:33:32 +0530

    Enable cors for hanger

--- AirPortWebApi/Controllers/HangerDetailsController.cs
+++ AirPortWebApi/Controllers/HangerDetailsController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AirPortWebApi.Controllers
 {
+    [EnableCors("*","*","*")]
     public class HangerDetailsController : ApiController
     {
         // GET api/<controller>
